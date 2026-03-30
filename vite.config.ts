834962aab4451f110e33b2b7ab4ab8a9c80130fc
Benjamin <7@toru.ca>
2026-03-30 15:31:58 -0400

    chore: consistency

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, "src/lib/index.ts"),
-      name: "Wikistxr",
+      name: "wikistxr",
     },
     rollupOptions: {
       output: [
