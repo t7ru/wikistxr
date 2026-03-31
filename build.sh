6131497e91e7a77c2b1da12474ada24bc2995d2a
Benjamin <gabonnie1234@gmail.com>
2026-03-31 12:59:17 -0400

    chore: build with simd

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -e
 
+export RUSTFLAGS="-C target-feature=+simd128"
+
 echo "Building..."
 cargo build --target wasm32-unknown-unknown --release
 wasm-bindgen --out-dir wasm --target web target/wasm32-unknown-unknown/release/wikistxr.wasm
-wasm-opt -Oz --enable-bulk-memory -o wasm/wikistxr_bg.wasm wasm/wikistxr_bg.wasm
+wasm-opt -Oz --enable-bulk-memory --enable-simd -o wasm/wikistxr_bg.wasm wasm/wikistxr_bg.wasm
 
 echo "Build complete, check wasm/"
